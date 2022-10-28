07e9156e2ee5d6e12760fd8ed4ba29735c954f66
Bruno Leone A P Correa <gerencia@acetime.com.br>
2022-10-27 22:37:16 -0300

    Add wallets endpoint

--- server.js
+++ server.js
@@ -7,7 +7,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use('/app', express.static(path.join(__dirname, '/public')));
+
+app.get('/wallets', (req, res) => { });
+
+app.post('/wallets', (req, res) => { });
+
+app.get('/wallets/:walletId', (req, res) => { });
+
+app.put('/wallets/:walletId', (req, res) => { });
+
+app.delete('/wallets/:walletId', (req, res) => { });
 
 let port = process.env.PORT || 3000;
 app.listen(port);
